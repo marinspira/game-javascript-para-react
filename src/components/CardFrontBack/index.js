89af4d901f9f1ab9e5186700ef711ca39d88fe11
Maria Eduarda Ferreira <maria.ferreira@feedz.com.br>
2023-02-03 15:26:56 -0300

    :sparkles: Add flip effect on click

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -2,8 +2,25 @@ import CardGame from "../CardGame";
 import './style.css'
 
 function CardFrontGameBack() {
+
+    window.cardFrontBack = {} // é um namespace, que torna a função global (window) handleClick única do CardFrontBack
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target;
+        const $cardFrontBack = $origin.closest(".card-front-back") // closest: todos os elementos que estiverem entre o elemento selecionado e o passado por parametro
+
+        /* 
+        if ($cardFrontBack.classList.contains("-active")) {
+            $cardFrontBack.classList.remove("-active")
+        } else {
+            $cardFrontBack.classList.add("-active");
+        } 
+        */
+
+        $cardFrontBack.classList.toggle("-active")
+    }
+
     return /*html*/`
-        <article class="card-front-back">
+        <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()}
             </div>
