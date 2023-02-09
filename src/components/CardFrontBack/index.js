1872c8bf0c037ed496e3c103808f76d6143bc21e
Maria Eduarda Ferreira <maria.ferreira@feedz.com.br>
2023-02-09 17:00:15 -0300

    :sparkles: Add functionality to untap cards and change players

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -4,7 +4,7 @@ import './style.css'
 function CardFrontBack(img) {
 
     window.cardFrontBack = {} // é um namespace, que torna a função global (window) handleClick única do CardFrontBack
-    window.cardFrontBack.handleClick = (event) => {
+    window.cardFrontBack.handleClick = (event) => { // função para remover e adicionar a classe active quando clicar (event.target - target identifica exatamente qual elemento foi clicado)
         const $origin = event.target;
         const $cardFrontBack = $origin.closest(".card-front-back") // closest: todos os elementos que estiverem entre o elemento selecionado e o passado por parametro
 
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -12,18 +12,39 @@ function shuffleArray(arr) {
 
 function BoardGame() {
 
+    // Embaralha as imagens
     const srcImg = ["blair1", "blair1", "blair2", "blair2", "blair3", "blair3"];
 
     const cardsRandow = shuffleArray(srcImg);
     let $htmlContent = '';
-    
+
     for (let i = 0; i < cardsRandow.length; i++) {
         const card = cardsRandow[i];
         $htmlContent += CardFrontBack(card);
     }
 
-    return `
-    <section class="board-game">
+    // Duas classes ativa: desvira os cards e muda o player
+    window.boardGame = {};
+    window.boardGame.handleClick = () => {
+        const $boardGame = document.querySelector('.board-game');
+        const $cardsActive = $boardGame.querySelectorAll('.card-front-back.-active')
+        const $arrowDown = document.querySelector('.arrow-down');
+
+        const flipAndHideCards = () => {
+            $cardsActive.forEach((card) => card.classList.remove('-active'));
+        } 
+
+        if ($cardsActive.length == 2) {
+            setTimeout(() => {
+                flipAndHideCards();
+                const currentPlayer = $arrowDown.getAttribute('data-currentplayer');
+                $arrowDown.setAttribute('data-currentplayer', currentPlayer == 1 ? 2 : 1);
+            }, 1000)
+        }
+    }
+
+    return /*html*/`
+    <section class="board-game" onClick="boardGame.handleClick()">
     ${$htmlContent}
     </section>
     `;
